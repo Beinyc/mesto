5324b2030b93115a5087d16e4b9f41885ebebe4f
Beiny <apatenko.nikita31rus@yandex.ru>
2023-02-11 17:38:00 +0300

    был изменен popup__opened на popup_opened

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let editProfileButton = document.querySelector('.profile__button-edit');
-let openPopup = document.querySelector('.popup__opened');
+let openPopup = document.querySelector('.popup_opened');
 let closeElementPopup = document.querySelector ('.popup__close');
 let popup = document.querySelector ('.popup');
 let formElement = document.querySelector ('.popup__container');
@@ -8,21 +8,21 @@ let jobInput = document.querySelector ('.popup__status');
 let nameElement = document.querySelector ('.profile__name');
 let jobElement = document.querySelector ('.profile__status');
 let closeProfileButton = document.querySelector('.popup__close');
-let closePopup = document.querySelector('.popup__opened');
+let closePopup = document.querySelector('.popup_opened');
 
 editProfileButton.addEventListener('click', function(){
-    openPopup.classList.remove('popup__opened');
+    openPopup.classList.remove('popup_opened');
 });
 
 closeProfileButton.addEventListener('click', function(){
-    closePopup.classList.add ('popup__opened');
+    closePopup.classList.add ('popup_opened');
 });
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameElement.textContent = nameInput.value;
     jobElement.textContent = jobInput.value;
-    closePopup.classList.add ('popup__opened');
+    closePopup.classList.add ('popup_opened');
 }
 
 formElement.addEventListener('submit',handleFormSubmit);
