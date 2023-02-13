8ab2152f46aa3912c502332d2ba300d710441d87
Beiny <apatenko.nikita31rus@yandex.ru>
2023-02-13 23:17:52 +0300

    Исправления для ревью №3

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
 let editProfileButton = document.querySelector('.profile__redact');
-let openPopup = document.querySelector('.popup_opened');
-let closeElementPopup = document.querySelector ('.popup__close');
-let popup = document.querySelector('.popup');
+let openPopup = document.querySelector('.popup');
+let closePopup = document.querySelector ('.popup');
 let formElement = document.querySelector('form[name="form1"]');
 let nameInput = document.querySelector('input[name="form1__name"]');
 let jobInput = document.querySelector('input[name="form1__status"]');
 let nameElement = document.querySelector(".profile__name");
 let jobElement = document.querySelector('.profile__status');
 let closeProfileButton = document.querySelector('.popup__close');
-let closePopup = document.querySelector('.popup_opened');
 
-editProfileButton.addEventListener('click', function(){
-    openPopup.classList.remove('popup_opened');
-});
+editProfileButton.addEventListener('click', OpenForm1);
 
-function closePopupButton (){
-    closePopup.classList.add ('popup_opened');
-}
+closeProfileButton.addEventListener('click', closeForm1);
 
-closeProfileButton.addEventListener('click', function(){
-    closePopup.classList.add ('popup_opened');
+function OpenForm1(){
+    openPopup.classList.add('popup_opened');
     nameInput.value = nameElement.textContent;
     jobInput.value = jobElement.textContent;
-});
+}
+
+function closeForm1(){
+    closePopup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameElement.textContent = nameInput.value;
     jobElement.textContent = jobInput.value;
-    closePopupButton();
+    closeForm1();
 }
 
 formElement.addEventListener('submit',handleFormSubmit);
